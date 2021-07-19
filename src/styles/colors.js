131e354cc394ad006ff1d0158858abb6466dead8
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-07-19 16:14:53 -0400

    create default theme layout

---
+++ src/styles/colors.js
@@ -0,0 +1,23 @@
+export const PRIMARY = "#1779ba";
+export const SECONDARY = "#767676";
+export const WHITE = "#FFFFFF";
+export const BLACK = "#111a2c";
+export const DARK_BLUE = "#363F5F";
+
+// THEME
+export const THEMECOLOR = "#2D2C2C";
+export const THEME_LIGHT = "#FFDED6";
+export const BACKGROUND_APP = "#FBFAFF";
+
+// ACTIONS
+export const SUCCESS = "#3adb76";
+export const WARNING = "#ffae00";
+export const ALERT = "#cc4b37";
+
+// GRAYSCALE
+export const GRAY_LIGHT = "#F5F5F8";
+export const GRAY_MEDIUM = "#cacaca";
+export const GRAY_DARK = "#7E7E7E";
+
+// TEXT
+export const TEXT_LABEL = "#727272";
