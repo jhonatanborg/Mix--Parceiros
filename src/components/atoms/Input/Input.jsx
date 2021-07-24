b59661ceedbce603b2b203d6c6a1484836fa487e
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-07-24 18:44:41 -0400

    input error app

--- src/components/atoms/Input/Input.jsx
+++ src/components/atoms/Input/Input.jsx
@@ -1,18 +1,33 @@
 import React from "react";
-import { View, TextInput, TextInputProps } from "react-native";
+import { View, TextInput, Text } from "react-native";
 import styles from "./Input.style";
-
-const InputText = ({ placeholder, numberOfLines, multiline, onChangeText }) => (
-  <View style={styles.sectionInput}>
-    <TextInput
-      style={styles.input}
-      underlineColorAndroid="transparent"
-      placeholder={placeholder}
-      placeholderTextColor="grey"
-      numberOfLines={numberOfLines}
-      multiline={multiline}
-      onChangeText={onChangeText}
-    />
-  </View>
-);
-export default InputText;
+export const InputText = ({
+  placeholder,
+  numberOfLines,
+  multiline,
+  onChangeText,
+  secureTextEntry,
+  error,
+}) => {
+  return (
+    <View style={styles.sectionInput}>
+      <Text style={styles.text}>{placeholder}</Text>
+      <View style={styles.row}>
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholderTextColor="grey"
+          numberOfLines={numberOfLines}
+          multiline={multiline}
+          onChangeText={onChangeText}
+          secureTextEntry={secureTextEntry}
+        />
+        {error && (
+          <View style={styles.error}>
+            <Text style={styles.errorText}>X</Text>
+          </View>
+        )}
+      </View>
+    </View>
+  );
+};
