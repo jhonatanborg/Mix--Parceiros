dfbd89a559f6cdbe9536c00ff745bd5dddcd2e5f
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-07-24 18:44:19 -0400

    Button loading request

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -1,11 +1,29 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
 import styles from "./Button.style";
+import { AntDesign } from "@expo/vector-icons";
 
-export default function Button({ onPress }) {
+export default function Button({ onPress, textButton, icon, isLoading }) {
+  if (isLoading) {
+    return (
+      <TouchableOpacity
+        disabled
+        activeOpacity={0.7}
+        onPress={onPress}
+        style={styles.loading}
+      >
+        <ActivityIndicator color="white" />
+      </TouchableOpacity>
+    );
+  }
   return (
-    <TouchableOpacity onPress={onPress} style={styles.button}>
-      <Text style={styles.text}>Confirmar</Text>
+    <TouchableOpacity
+      activeOpacity={0.7}
+      onPress={onPress}
+      style={styles.button}
+    >
+      <Text style={styles.text}>{textButton}</Text>
+      {icon && <AntDesign name="arrowright" size={24} color="white" />}
     </TouchableOpacity>
   );
 }
--- src/components/atoms/Button/Button.style.js
+++ src/components/atoms/Button/Button.style.js
@@ -4,15 +4,26 @@ import { Typography, Colors } from "../../../styles";
 const styles = StyleSheet.create({
   button: {
     flexDirection: "row",
-    justifyContent: "center",
-    paddingVertical: 25,
+    justifyContent: "space-between",
+    paddingVertical: 15,
     backgroundColor: Colors.THEMECOLOR,
     borderRadius: 5,
+    paddingHorizontal: 25,
+    alignItems: "center",
+  },
+  loading: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingVertical: 15,
+    backgroundColor: Colors.THEMECOLOR,
+    borderRadius: 5,
+    paddingHorizontal: 25,
+    alignItems: "center",
   },
   text: {
     color: Colors.WHITE,
     fontFamily: Typography.FONT_FAMILY_BOLD,
-    fontSize: Typography.FONT_SIZE_22,
+    fontSize: Typography.FONT_SIZE_12,
   },
 });
 export default styles;
