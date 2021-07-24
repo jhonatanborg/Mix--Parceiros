a2941a3e5f372fc544e0940bfea2b329c17427a5
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-07-24 18:46:31 -0400

    Home timeout request useEffect

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,52 +1,107 @@
-import React, { useEffect, useState, useContext } from "react";
-import { Text, View, ScrollView, ActivityIndicator } from "react-native";
+import React, { useEffect, useState, useContext, useRef } from "react";
+import {
+  Text,
+  View,
+  FlatList,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import Card from "../../components/molecules/Card/Card";
+import { Ionicons } from "@expo/vector-icons";
+import { BottomSheet } from "../../components/molecules/BottomSheet/BottomSheet";
 import Header from "../../components/molecules/Header/Header";
 import AuthContext from "../../contexts/session";
 import { Purchase } from "../../services";
+import { statusPurchase } from "../../shared/purchase";
 import styles from "./Home.style";
-export default function Home(props) {
+export default function Home({ navigation }) {
   const { user } = useContext(AuthContext);
   const [purchases, setPurchases] = useState([]);
-  const [loading, setLoading] = useState(true);
-  function navigate(key) {
-    props.navigation.navigate("Details", key);
+  const [status, setStatus] = useState("Pendente");
+  const [bottom, setBottom] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  function navigate(item) {
+    navigation.navigate("Details", item);
   }
+  function fetchFilter(filter) {
+    setStatus(filter);
+  }
+
   useEffect(() => {
-    async function fetchPurchases() {
-      const response = await Purchase.listPurchases();
-      setPurchases(response.data);
-      setLoading(false);
-    }
+    setIsLoading(true);
+
+    const fetchPurchases = async () => {
+      try {
+        const response = await Purchase.listPurchases(status);
+        const listPurchases = [];
+        response.data.forEach((item) => {
+          listPurchases.push({
+            id: item.id,
+            client_name: item.client?.name,
+            date: item.created_at,
+            total: item.total,
+            quantity: item.itens.length,
+            status: statusPurchase(item.status),
+            itens: item.itens,
+            address: item.deliveryAddress,
+          });
+        });
+        setPurchases(listPurchases);
+        setIsLoading(false);
+        setBottom(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+        setBottom(false);
+      }
+    };
     const timeout = setTimeout(() => {
       fetchPurchases();
     }, 5000);
     return () => clearTimeout(timeout);
-  }, []);
-
+  }, [status]);
   return (
-    <ScrollView style={styles.container}>
-      <Header name={user?.name} />
-      <View style={styles.section}>
-        <Text style={styles.heading1}>Lista de pedidos</Text>
-        {loading ? (
-          <ActivityIndicator size="large" color="#000" />
-        ) : (
-          purchases.map((item, key) => {
-            return (
+    <>
+      <ScrollView style={styles.container}>
+        <Header name={user?.name} />
+        <View style={styles.section}>
+          <View style={styles.flex}>
+            <Text style={styles.heading1}>Lista de pedidos</Text>
+            <TouchableOpacity onPress={() => setBottom(true)}>
+              <Ionicons name="ios-filter-outline" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+
+          <FlatList
+            data={purchases}
+            progressViewOffset={5}
+            refreshing={isLoading}
+            onEndReachedThreshold={0.1}
+            renderItem={({ item }) => (
               <Card
-                navigate={(id) => navigate(id)}
-                company_name={item.client.name}
+                navigate={(id) => navigate(item)}
+                client_name={item.client_name}
                 id={item.id}
-                data={item.created_at}
+                date={item.date}
                 total={item.total}
-                quantity={item.itens.length}
-                status={item.status}
+                quantity={item.quantity}
+                status={item.status.title}
+                colorPrimary={item.status.color.primary}
+                colorSecondary={item.status.color.secondary}
               />
-            );
-          })
-        )}
-      </View>
-    </ScrollView>
+            )}
+            extraData={purchases}
+            keyExtractor={(item, key) => item.id + key.toString()}
+          />
+        </View>
+      </ScrollView>
+      <BottomSheet
+        closeModal={() => setBottom(false)}
+        isClose={isLoading}
+        openBottom={bottom}
+        isLoading={isLoading}
+        fetchFilter={(value) => fetchFilter(value)}
+      />
+    </>
   );
 }
--- src/screens/home/Home.style.js
+++ src/screens/home/Home.style.js
@@ -4,17 +4,26 @@ import { Typography, Colors } from "../../styles";
 const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.BACKGROUND_APP,
-    flex: 1,
   },
   section: {
     paddingHorizontal: 16,
     paddingVertical: 16,
   },
+  flex: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
   heading1: {
     color: Colors.DARK_BLUE,
     fontFamily: Typography.FONT_FAMILY_REGULAR,
-    fontSize: Typography.FONT_SIZE_22,
+    fontSize: Typography.FONT_SIZE_16,
     paddingVertical: 8,
+  },
+  loading: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "orange",
   },
 });
 export default styles;
