c7706b60fde1b39d9795ece3697471cc83fdf0ea
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-07-24 18:48:03 -0400

    styles app theme color

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -5,14 +5,15 @@ export const BLACK = "#111a2c";
 export const DARK_BLUE = "#363F5F";
 
 // THEME
-export const THEMECOLOR = "#2D2C2C";
+export const THEMECOLOR = "#FF6C44";
 export const THEME_LIGHT = "#FFDED6";
 export const BACKGROUND_APP = "#FBFAFF";
 
 // ACTIONS
 export const SUCCESS = "#3adb76";
 export const WARNING = "#ffae00";
-export const ALERT = "#cc4b37";
+export const ALERT = "#e94e2c";
+export const ALERT_BACKGROUND = "#fcede9";
 
 // GRAYSCALE
 export const GRAY_LIGHT = "#F5F5F8";
--- src/utils/index.js
+++ src/utils/index.js
@@ -24,5 +24,8 @@ const convertMoney = (
       : "")
   );
 };
-
-export { convertMoney };
+const validEmail = (email) => {
+  var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return pattern.test(email);
+};
+export { convertMoney, validEmail };
