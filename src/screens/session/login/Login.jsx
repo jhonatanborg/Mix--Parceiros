a523bea32e55385445d6e810b23556f973a00c62
jhonatanborg <jhonatanborgesdj@gmail.com>
2021-07-24 18:47:22 -0400

    Login useform valid data

--- src/screens/session/login/Login.jsx
+++ src/screens/session/login/Login.jsx
@@ -1,49 +1,65 @@
 import React, { useState, useContext } from "react";
-import { View, KeyboardAvoidingView, Platform, Image } from "react-native";
+import {
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  Image,
+  Text,
+} from "react-native";
 import Button from "../../../components/atoms/Button/Button";
-import InputText from "../../../components/atoms/Input/Input";
+import { InputText } from "../../../components/atoms/Input/Input";
 import styles from "./Login.style";
-import { useAuth } from "../../../contexts/session.jsx";
 import AuthContext from "../../../contexts/session";
-
+import logo from "../../../assets/logo-app.png";
+import { useLoginFormState } from "../../../hooks/useForm";
+import { Error } from "../../../components/atoms/Error/Error";
 export default function Login() {
-  const [email, setLogin] = useState("emporiodaeconomia@gmail.com");
-  const [password, setPassword] = useState("001001001");
+  const { email, password, submit } = useLoginFormState();
+  const [error, setError] = useState(false);
   const { login } = useContext(AuthContext);
   async function handleSignIn() {
-    const response = await login({ email, password, cpanel: true });
-    console.log("resposta", response);
+    console.log(submit.value);
+
+    if (submit.value) {
+      const response = await login({
+        email: email.value,
+        password: password.value,
+        cpanel: true,
+      })
+        .then()
+        .catch((err) => setError(true));
+    }
   }
   return (
     <View style={styles.container}>
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
       >
-        <View>
-          <Image
-            source={{
-              uri: "https://admin.bransa.com.br/img/logo.a1450bed.png",
-            }}
-            style={styles.tinyLogo}
-          />
-          <View style={styles.input}>
-            <InputText
-              numberOfLines={1}
-              multiline={false}
-              placeholder="Email"
-              onChangeText={(value) => setLogin(value)}
-            />
-          </View>
-          <View style={styles.input}>
-            <InputText
-              numberOfLines={1}
-              multiline={false}
-              placeholder="Senha"
-              onChangeText={(value) => setPassword(value)}
-            />
-          </View>
-          <Button onPress={() => handleSignIn()} />
+        <View style={styles.flex}>
+          <Image source={logo} style={styles.tinyLogo} />
+          <Text style={styles.title}>Mix - delivery</Text>
         </View>
+        <View style={styles.input}>
+          <InputText
+            numberOfLines={1}
+            multiline={false}
+            placeholder="Email"
+            onChangeText={(value) => email.set(value)}
+            error={submit.value && !email.valid}
+          />
+        </View>
+        <View style={styles.input}>
+          <InputText
+            numberOfLines={1}
+            multiline={false}
+            placeholder="Senha"
+            onChangeText={(value) => password.set(value)}
+            secureTextEntry
+            error={submit.value && !password.valid}
+          />
+        </View>
+        <Error message="Usuário não encontrado" error={error} />
+        <Button icon onPress={() => handleSignIn()} textButton="Entrar" />
       </KeyboardAvoidingView>
     </View>
   );
--- src/screens/session/login/Login.style.js
+++ src/screens/session/login/Login.style.js
@@ -15,6 +15,16 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
   },
+  flex: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: Typography.FONT_SIZE_22,
+    fontFamily: Typography.FONT_FAMILY_BOLD,
+    color: Colors.THEMECOLOR,
+    marginHorizontal: Spacing.SCALE_18,
+  },
 });
 
 export default styles;
